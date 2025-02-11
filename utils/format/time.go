d1e443edf300b94e0735b5dbf011fa862cc0bea5
ericwyn <16485562+Ericwyn@users.noreply.github.com>
2025-02-11 17:11:29 +0800

    feat: update time smart parse rule

--- utils/format/time.go
+++ utils/format/time.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/Ericwyn/EzeFormat/log"
 	"github.com/Ericwyn/GoTools/date"
+	"github.com/Ericwyn/GoTools/str"
 	"strconv"
 	"time"
 )
@@ -51,7 +52,7 @@ func formatTimeStamp(input string) (string, error) {
 }
 
 func formatDate(guessType GuessStrType, input string) (string, error) {
-	t, err := date.ToTime(input, timeParseMap[guessType])
+	t, err := bjTimeYyyyMMddToTimeStamp(input, timeParseMap[guessType])
 	if err != nil {
 		return input, errors.New("按照日期格式 " + timeParseMap[guessType] + " 解析失败")
 	}
@@ -59,12 +60,30 @@ func formatDate(guessType GuessStrType, input string) (string, error) {
 	return formatTime(t)
 }
 
+// bjTimeYyyyMMddToTimeStamp
+// 所有 yyyyMMdd 之类的时间字符串, 这里都默认为北京时间来解析成 time.Time
+func bjTimeYyyyMMddToTimeStamp(timeStr string, formatStr string) (time.Time, error) {
+	formatStr = getFormatString(formatStr)
+
+	// 加载 UTC+8 时区
+	location, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	// 直接在 UTC+8 时区解析时间
+	parsedTime, err := time.ParseInLocation(formatStr, timeStr, location)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return parsedTime, nil
+}
+
 // formatTime 格式化时间
 // 将一个时间用多种格式进行格式化，然后返回
 func formatTime(t time.Time) (string, error) {
 	var result = "[time parse]\n"
-
-	result += "\n"
 
 	// 时间戳格式
 	// t 转为 10位时间戳
@@ -74,11 +93,49 @@ func formatTime(t time.Time) (string, error) {
 
 	result += "\n"
 
-	// 年月日格式
-	result += date.Format(t, timeParseMap[TypeData]) + "\n"
-	result += date.Format(t, timeParseMap[TypeDateTimeMinutes]) + "\n"
-	result += date.Format(t, timeParseMap[TypeDateTimeSeconds]) + "\n"
-	result += date.Format(t, timeParseMap[TypeDateTimeMills]) + "\n"
+	// UTC+0 时间格式
+	utcTime := t.UTC()
+	result += "UTC+0:\n"
+	result += date.Format(utcTime, timeParseMap[TypeData]) + "\n"
+	result += date.Format(utcTime, timeParseMap[TypeDateTimeMinutes]) + "\n"
+	result += date.Format(utcTime, timeParseMap[TypeDateTimeSeconds]) + "\n"
+	result += date.Format(utcTime, timeParseMap[TypeDateTimeMills]) + "\n"
+
+	result += "\n"
+
+	// 北京时间格式 (UTC+8)
+	beijingLocation, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return "", err
+	}
+	beijingTime := t.In(beijingLocation)
+	result += "UTC+8:\n"
+	result += date.Format(beijingTime, timeParseMap[TypeData]) + "\n"
+	result += date.Format(beijingTime, timeParseMap[TypeDateTimeMinutes]) + "\n"
+	result += date.Format(beijingTime, timeParseMap[TypeDateTimeSeconds]) + "\n"
+	result += date.Format(beijingTime, timeParseMap[TypeDateTimeMills]) + "\n"
 
 	return result, nil
+}
+
+var dateFormatMap = map[string]string{}
+
+// 传入 yyyyMMdd 之类的
+func getFormatString(formatStr string) string {
+	// 原始 format string
+	formatStrOld := formatStr
+	// 构建一个 formatStr
+	if formatStr := dateFormatMap[formatStrOld]; formatStr == "" {
+		formatStr = str.ReplaceAll(formatStrOld, "yyyy", "2006")
+		formatStr = str.ReplaceAll(formatStr, "yy", "06")
+		formatStr = str.ReplaceAll(formatStr, "MM", "01")
+		formatStr = str.ReplaceAll(formatStr, "dd", "02")
+		formatStr = str.ReplaceAll(formatStr, "HH", "15")
+		formatStr = str.ReplaceAll(formatStr, "hh", "3")
+		formatStr = str.ReplaceAll(formatStr, "mm", "04")
+		formatStr = str.ReplaceAll(formatStr, "ss", "05")
+		formatStr = str.ReplaceAll(formatStr, "SSS", "000")
+		dateFormatMap[formatStrOld] = formatStr
+	}
+	return dateFormatMap[formatStrOld]
 }
